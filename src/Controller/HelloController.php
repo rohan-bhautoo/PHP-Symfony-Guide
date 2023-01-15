28052554f536e6155e78bf567f3ff0e83fcb8641
Rohan Bhautoo <rohanbhautoo@gmail.com>
2023-01-15 17:59:56 +0400

    Controllers + Attributes Routing

---
+++ src/Controller/HelloController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HelloController extends AbstractController
+{
+    #[Route('/hello', name: 'app_hello')]
+    public function index(): Response
+    {
+        return $this->render('hello/index.html.twig', [
+            'controller_name' => 'HelloController',
+        ]);
+    }
+
+    #[Route('/hello/{id}', name: 'app_id')]
+    public function showId($id): Response
+    {
+        return new Response($id);
+    }
+}
