8dbce94a096f1f78f98eb4963ebde5232a9cd199
Rohan Bhautoo <rohanbhautoo@gmail.com>
2023-01-24 17:44:11 +0400

    Flash Message & Redirect

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -47,8 +47,10 @@ class MicroPostController extends AbstractController
             $microPostRepository->save($microPost, true);
 
             // Flash Message
+            $this->addFlash('success', 'Your micro post has been added!');
 
             // Redirect Page
+            return $this->redirectToRoute('app_micro_post');
         }
 
         return $this->render('micro_post/_add.html.twig', [
